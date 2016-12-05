be470645e48ef6286482dafff97c7c27d1e7d0d7
William Knight <kniwilliam@gmail.com>
2016-12-05 11:38:04 -0800

    Add routes component

---
+++ app/components/routes.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { browserHistory, IndexRoute, Route, Router } from 'react-router';
+
+const Routes = React.createClass({
+  render() {
+    return <Router
+      history={browserHistory}
+    >
+    
+    </Router>
+  }
+})
